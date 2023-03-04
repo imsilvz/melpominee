60c4d2d1a9b4d78d9424952910267c1d28e5986f
Richard Yawger <rjyawger@me.com>
2023-03-03 22:19:00 -0500

    initial file structure

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import path from 'path';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: path.resolve(__dirname, 'frontend'),
+  plugins: [react()]
+});

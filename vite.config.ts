95bb77b24e62155f3bef18378a7e2c3ee431d941
Richard Yawger <rjyawger@me.com>
2023-03-03 23:26:16 -0500

    eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import path from 'path';
 
 import { defineConfig } from 'vite';
@@ -6,5 +7,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   root: path.resolve(__dirname, 'frontend'),
-  plugins: [react()]
+  plugins: [react()],
 });
